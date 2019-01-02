14e9860cd77f111ef33696359f51e73919c6c7e3
Mayionn <43456657+Mayionn@users.noreply.github.com>
2019-01-02 19:06:50 +0000

    Added Objects + Updated interface

---
+++ Projeto de Jogos Sérios/Assets/Scripts/VisibleOrInvisibleOnClick.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VisibleOrInvisibleOnClick : MonoBehaviour {
+
+    private Transform originalPosition;
+
+    public GameObject prefab;
+    public Button m_button;
+
+    void Start()
+    {
+        m_button.onClick.AddListener(TaskOnClick);
+    }
+
+    void TaskOnClick()
+    {
+            if (prefab.activeInHierarchy)
+            {
+                prefab.SetActive(false);
+            Vector3 currentPosition = new Vector3 (transform.position.x, transform.position.y, transform.position.z);
+            Quaternion currentRotation = new Quaternion (transform.rotation.x, transform.rotation.y, transform.rotation.z, 0);
+            } else
+            {
+                prefab.SetActive(true);
+            }
+    }
+}
