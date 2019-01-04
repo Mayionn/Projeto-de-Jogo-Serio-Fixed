0dde1455719ec99f780abdb79bd10c07430982c6
Mayionn <43456657+Mayionn@users.noreply.github.com>
2019-01-04 16:14:15 +0000

    Selected and Unseleted Tabs

---
+++ Projeto de Jogos Sérios/Assets/Scripts/TabsChangeSprite.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TabsChangeSprite : MonoBehaviour {
+
+    public Button ambiente;
+    public Button alimento;
+
+    public Sprite tabSelected;
+    public Sprite tabUnselected;
+
+	// Use this for initialization
+	void Start ()
+    {
+        ambiente.onClick.AddListener(AmbienteOnClick);
+        alimento.onClick.AddListener(AlimentoOnClick);
+	}
+	
+	void AmbienteOnClick ()
+    {
+        alimento.GetComponent<Image>().overrideSprite = tabUnselected;
+        ambiente.GetComponent<Image>().overrideSprite = tabSelected;
+	}
+
+    void AlimentoOnClick ()
+    {
+        ambiente.GetComponent<Image>().overrideSprite = tabUnselected;
+        alimento.GetComponent<Image>().overrideSprite = tabSelected;
+    }
+}
