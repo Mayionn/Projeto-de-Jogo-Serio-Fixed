5862b87a773e2de6e5c27e5824fe5f64f4d5359c
Mayionn <43456657+Mayionn@users.noreply.github.com>
2019-01-07 17:13:51 +0000

    Explanations on level one fully working
    Added explanation box to level 2 but thats it

--- Projeto de Jogos Sérios/Assets/Scripts/Explanations1.cs
+++ Projeto de Jogos Sérios/Assets/Scripts/Explanations1.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Explanations1 : MonoBehaviour {
+public class Explanations1 : MonoBehaviour
+{
 
     public GameObject[] answersCorrect = { };
     public GameObject[] answersIncorrect = { };
@@ -19,13 +20,14 @@ public class Explanations1 : MonoBehaviour {
         {
             Default.SetActive(false);
             explanationCorrect[0].SetActive(true);
-        } else
+        }
+        else
         {
             Default.SetActive(true);
             explanationCorrect[0].SetActive(false);
         }
     }
-    
+
     public void mouseExitCorrect1()
     {
         if (answersCorrect[0].activeInHierarchy)
@@ -38,10 +40,11 @@ public class Explanations1 : MonoBehaviour {
     public void mouseEnterCorrect2()
     {
         if (answersCorrect[1].activeInHierarchy)
-        { 
+        {
             Default.SetActive(false);
             explanationCorrect[1].SetActive(true);
-        } else
+        }
+        else
         {
             Default.SetActive(true);
             explanationCorrect[1].SetActive(false);
@@ -54,6 +57,108 @@ public class Explanations1 : MonoBehaviour {
         {
             Default.SetActive(true);
             explanationCorrect[1].SetActive(false);
+        }
+    }
+
+    public void mouseEnterCorrect3()
+    {
+        if (answersCorrect[2].activeInHierarchy)
+        {
+            Default.SetActive(false);
+            explanationCorrect[2].SetActive(true);
+        }
+        else
+        {
+            Default.SetActive(true);
+            explanationCorrect[2].SetActive(false);
+        }
+    }
+
+    public void mouseExitCorrect3()
+    {
+        if (answersCorrect[2].activeInHierarchy)
+        {
+            Default.SetActive(true);
+            explanationCorrect[2].SetActive(false);
+        }
+    }
+
+    public void mouseEnterCorrect4()
+    {
+        if (answersCorrect[3].activeInHierarchy)
+        {
+            Default.SetActive(false);
+            explanationCorrect[3].SetActive(true);
+        }
+        else
+        {
+            Default.SetActive(true);
+            explanationCorrect[3].SetActive(false);
+        }
+    }
+
+    public void mouseExitCorrect4()
+    {
+        if (answersCorrect[3].activeInHierarchy)
+        {
+            Default.SetActive(true);
+            explanationCorrect[3].SetActive(false);
+        }
+    }
+
+
+
+    public void mouseEnterIncorrect1()
+    {
+        if (answersIncorrect[0].activeInHierarchy)
+        {
+            Default.SetActive(false);
+            explanationIncorrect[0].SetActive(true);
+        }
+    }
+
+    public void mouseExitIncorrect1()
+    {
+        if (answersIncorrect[0].activeInHierarchy)
+        {
+            Default.SetActive(true);
+            explanationIncorrect[0].SetActive(false);
+        }
+    }
+
+    public void mouseEnterIncorrect2()
+    {
+        if (answersIncorrect[1].activeInHierarchy)
+        {
+            Default.SetActive(false);
+            explanationIncorrect[1].SetActive(true);
+        }
+    }
+
+    public void mouseExitIncorrect2()
+    {
+        if (answersIncorrect[1].activeInHierarchy)
+        {
+            Default.SetActive(true);
+            explanationIncorrect[1].SetActive(false);
+        }
+    }
+
+    public void mouseEnterIncorrect3()
+    {
+        if (answersIncorrect[2].activeInHierarchy)
+        {
+            Default.SetActive(false);
+            explanationIncorrect[2].SetActive(true);
+        }
+    }
+
+    public void mouseExitIncorrect3()
+    {
+        if (answersIncorrect[2].activeInHierarchy)
+        {
+            Default.SetActive(true);
+            explanationIncorrect[2].SetActive(false);
         }
     }
 }
