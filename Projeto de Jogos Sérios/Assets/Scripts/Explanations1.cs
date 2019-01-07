36b2e9693423b77bd16e568ffdd4d9c446d37181
Mayionn <43456657+Mayionn@users.noreply.github.com>
2019-01-07 12:29:57 +0000

    Explicação das respostas
    Tweak à interface para aparecer o motivo do porque de estarem as respostas corretas ou incorretas. Organização de algumas files

---
+++ Projeto de Jogos Sérios/Assets/Scripts/Explanations1.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Explanations1 : MonoBehaviour {
+
+    public GameObject[] answersCorrect = { };
+    public GameObject[] answersIncorrect = { };
+
+    public GameObject[] explanationCorrect = { };
+    public GameObject[] explanationIncorrect = { };
+
+    public GameObject Default;
+
+    public void mouseEnterCorrect1()
+    {
+        if (answersCorrect[0].activeInHierarchy)
+        {
+            Default.SetActive(false);
+            explanationCorrect[0].SetActive(true);
+        } else
+        {
+            Default.SetActive(true);
+            explanationCorrect[0].SetActive(false);
+        }
+    }
+    
+    public void mouseExitCorrect1()
+    {
+        if (answersCorrect[0].activeInHierarchy)
+        {
+            Default.SetActive(true);
+            explanationCorrect[0].SetActive(false);
+        }
+    }
+
+    public void mouseEnterCorrect2()
+    {
+        if (answersCorrect[1].activeInHierarchy)
+        { 
+            Default.SetActive(false);
+            explanationCorrect[1].SetActive(true);
+        } else
+        {
+            Default.SetActive(true);
+            explanationCorrect[1].SetActive(false);
+        }
+    }
+
+    public void mouseExitCorrect2()
+    {
+        if (answersCorrect[1].activeInHierarchy)
+        {
+            Default.SetActive(true);
+            explanationCorrect[1].SetActive(false);
+        }
+    }
+}
